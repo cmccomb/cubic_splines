f47dc6de68301b6dda98c013c2b2dee1c58d0f19
Chris McComb <ccmcc2012@gmail.com>
2015-02-20 08:14:27 -0500

    Create cubic_spline.m

---
+++ cubic_spline.m
@@ -0,0 +1,36 @@
+function f = cubic_spline(x, y)
+% CUBIC_SPLINE Cubic splines fit to a data set
+% For CUBIC_SPLINE(X, Y)
+%   Inputs:
+%     X: a vector of values for the independent variable
+%     Y: a vector of values for the dependent variable
+%   Outputs: 
+%     f: an inline function that computes the fitted piecewise cubic polynomial
+
+    %create matrix
+    N = length(x);
+    delta = abs(x(1)-x(2));
+    for i=1:1:N
+        if i==1
+            A(1,1) = 1;
+            B(1) = 0;
+        elseif i==N
+            A(N, N) = 1;
+            B(N) = 0;
+        else
+            A(i,i-1) = 1/6;
+            A(i,i) = 2/3;
+            A(i,i+1) = 1/6;
+            B(i) = (y(i+1)-2*y(i)+y(i-1))/(delta^2);
+        end
+    end
+            
+    %solve matrix
+    G = A\(B');
+    
+    % Create function
+    j = @(yy) interp1(x, 1:1:N, yy, 'previous'); 
+    f = @(xx) (G(j(xx))/6)*(((x(j(xx)+1)-xx)^3)/delta-delta*(x(j(xx)+1)-xx)) ...
+        + (G(j(xx)+1)/6)*(((xx-x(j(xx)))^3)/delta-delta*(xx-x(j(xx)))) ...
+        + y(j(xx))*(x(j(xx)+1)-xx)/delta + y(j(xx)+1)*(xx-x(j(xx)))/delta;
+end
